326860487e81e79a4b6f9a1103ae8d0b4b93f551
mannu chaurasiya <mannuchaurasiya36@gmail.com>
2023-10-31 18:16:54 +0530

    adding some practices

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// const service = document.querySelector("a[href='#services']")
+
+document.querySelector("a[href='#services']").addEventListener("click", function(){
+ alert("Please login and Signup to Access the features")
+});
